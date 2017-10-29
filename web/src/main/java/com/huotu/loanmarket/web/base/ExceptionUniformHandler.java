ffbdfa9f900d678c469132454814e58d2c778758
allan <lqc_huli@163.com>
2017-10-30 00:47:26 +0800

    增加了统一异常处理器

---
+++ web/src/main/java/com/huotu/loanmarket/web/base/ExceptionUniformHandler.java
@@ -0,0 +1,31 @@
+package com.huotu.loanmarket.web.base;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 异常同意处理器
+ *
+ * @author allan
+ * @date 30/10/2017
+ */
+@Controller
+@ControllerAdvice
+public class ExceptionUniformHandler {
+    private static final Log log = LogFactory.getLog(ExceptionUniformHandler.class);
+
+    /**
+     * 所有异常统一处理
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ApiResult runtimeExceptionHandler(Exception ex) {
+        log.error(ex);
+        log.info("exception:" + ex.getMessage());
+        return ApiResult.resultWith(ResultCodeEnum.SYSTEM_BAD_REQUEST, ex.getMessage(), null);
+    }
+}
