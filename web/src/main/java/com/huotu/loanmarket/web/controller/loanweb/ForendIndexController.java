379d630f891eab53b37950b7c972b43f5b20d02f
huxuanhao2017 <hu_xuanhao@163.com>
2017-10-30 17:45:49 +0800

    修改页面路径

--- web/src/main/java/com/huotu/loanmarket/web/controller/loanweb/ForendIndexController.java
+++ web/src/main/java/com/huotu/loanmarket/web/controller/loanweb/ForendIndexController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ForendIndexController {
     @RequestMapping("/index")
     public String index(){
-       return "home";
+       return "forend/home";
     }
 }
 
